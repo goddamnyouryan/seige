d095024b9772d0773a0ff6e94197bda5d7e00994
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-08-11 15:04:42 -0700

    Adding basic voxel support.

---
+++ Assets/VoxelGrid.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]
+public class VoxelGrid : MonoBehaviour
+{
+    public int resolution;
+    public GameObject voxelPrefab;
+
+    private bool[] voxels;
+    private float voxelSize;
+    private Material[] voxelMaterials;
+
+    public void Initialize(int resolution, float size) {
+        this.resolution = resolution;
+        voxelSize = size / resolution;
+        voxels = new bool[resolution * resolution];
+        voxelMaterials = new Material[voxels.Length];
+
+        for (int i = 0, y = 0; y < resolution; y++) {
+            for (int x = 0; x < resolution; x++, i++) {
+                CreateVoxel(i, x, y);
+            }
+        }
+
+        SetVoxelColors();
+    }
+
+    private void CreateVoxel(int i, int x, int y) {
+        GameObject o = Instantiate(voxelPrefab) as GameObject;
+        o.transform.parent = transform;
+        o.transform.localPosition = new Vector3((x + 0.5f) *  voxelSize, (y + 0.5f) * voxelSize);
+        o.transform.localScale = Vector3.one  * voxelSize * 0.9f;
+        voxelMaterials[i] = o.GetComponent<MeshRenderer>().material;
+    }
+
+    public void SetVoxel(int x, int y, bool state) {
+        voxels[y * resolution + x] = state;
+        SetVoxelColors();
+    }
+
+    private void SetVoxelColors() {
+        for (int i = 0; i < voxels.Length; i++) {
+            voxelMaterials[i].color = voxels[i] ? Color.black : Color.white;
+        }
+    }
+}
---
+++ Assets/VoxelMap.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelMap : MonoBehaviour
+{
+    public float size = 2f;
+    public int voxelResolution = 8;
+    public int chunkResolution = 2;
+    public VoxelGrid voxelGridPrefab;
+
+    private VoxelGrid[] chunks;
+    private float chunkSize, voxelSize, halfSize;
+
+    private void Awake() {
+        halfSize = size * 0.5f;
+        chunkSize = size / chunkResolution;
+        voxelSize = chunkSize / voxelResolution;
+
+        chunks = new VoxelGrid[chunkResolution * chunkResolution];
+        for (int i = 0, y = 0; y < chunkResolution; y++) {
+            for (int x = 0; x < chunkResolution; x++, i++) {
+                CreateChunk(i, x, y);
+            }
+        }
+
+        BoxCollider box = gameObject.AddComponent<BoxCollider>();
+        box.size = new Vector3(size, size);
+    }
+
+    private void CreateChunk(int i, int x, int y) {
+        VoxelGrid chunk = Instantiate(voxelGridPrefab) as VoxelGrid;
+        chunk.Initialize(voxelResolution, chunkSize);
+        chunk.transform.parent = transform;
+        chunk.transform.localPosition = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize);
+        chunks[i] = chunk;
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            RaycastHit hitInfo;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo)) {
+                if (hitInfo.collider.gameObject == gameObject) {
+                    EditVoxels(transform.InverseTransformPoint(hitInfo.point));
+                }
+            }
+        }
+    }
+
+    private void EditVoxels(Vector3 point) {
+        int voxelX = (int)((point.x + halfSize) / voxelSize);
+        int voxelY = (int)((point.y + halfSize) / voxelSize);
+        int chunkX = voxelX / voxelResolution;
+        int chunkY = voxelY / voxelResolution;
+        voxelX -= chunkX * voxelResolution;
+        voxelY -= chunkY * voxelResolution;
+
+        chunks[chunkY * chunkResolution + chunkX].SetVoxel(voxelX, voxelY, true);
+    }
+}
