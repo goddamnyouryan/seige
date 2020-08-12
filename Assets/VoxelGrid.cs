6513315fc6ec3be1728c033873b42f6f106b2112
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-08-11 17:57:28 -0700

    Fixing issue with prefab.

--- Assets/VoxelGrid.cs
+++ Assets/VoxelGrid.cs
@@ -13,6 +13,7 @@ public class VoxelGrid : MonoBehaviour
     private Material[] voxelMaterials;
 
     public void Initialize(int resolution, float size) {
+        Console.Log("Initialize", resolution, size);
         this.resolution = resolution;
         voxelSize = size / resolution;
         voxels = new bool[resolution * resolution];
@@ -31,18 +32,24 @@ public class VoxelGrid : MonoBehaviour
         GameObject o = Instantiate(voxelPrefab) as GameObject;
         o.transform.parent = transform;
         o.transform.localPosition = new Vector3((x + 0.5f) *  voxelSize, (y + 0.5f) * voxelSize);
-        o.transform.localScale = Vector3.one  * voxelSize * 0.9f;
+        o.transform.localScale = Vector3.one  * voxelSize;
         voxelMaterials[i] = o.GetComponent<MeshRenderer>().material;
     }
 
     public void SetVoxel(int x, int y, bool state) {
+        // LogColor(y * resolution + x);
         voxels[y * resolution + x] = state;
         SetVoxelColors();
+
     }
 
     private void SetVoxelColors() {
         for (int i = 0; i < voxels.Length; i++) {
-            voxelMaterials[i].color = voxels[i] ? Color.black : Color.white;
+            voxelMaterials[i].color = voxels[i] ? Color.red : Color.white;
         }
+    }
+
+    private void LogColor(int i) {
+        Console.Log(i, voxelMaterials[i].color, voxels[i]);
     }
 }
