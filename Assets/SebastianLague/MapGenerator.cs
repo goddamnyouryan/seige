8089368de224375e703360ac1afe3d8566c118cd
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-08-17 18:20:56 -0700

    Getting started on sebastian lague marching cubes.

---
+++ Assets/SebastianLague/MapGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width, height;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+    int[,] map;
+
+    void Start() {
+        GenerateMap();
+    }
+
+    void GenerateMap() {
+        map = new int[width, height];
+    }
+
+}
